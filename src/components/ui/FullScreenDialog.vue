4dc36aebf3dbbcc2e249572dee9755125ce4c2c8
myatthu-mm <myatthu2018@ucsy.edu.mm>
2020-09-19 01:47:33 +0630

    optimized fullscreen dialog

---
+++ src/components/ui/FullScreenDialog.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <q-dialog
+      v-model="isOpen"
+      persistent
+      maximized
+      transition-show="slide-up"
+      transition-hide="slide-down"
+    >
+      <q-card class="text-white" style="background: #f44336;
+  font-family: 'Acme', sans-serif;">
+        <q-bar>
+          <q-space />
+          <q-btn dense flat icon="close" v-close-popup @click="closeDialog">
+            <q-tooltip content-class="bg-white text-primary">Close</q-tooltip>
+          </q-btn>
+        </q-bar>
+
+        <q-card-section>
+          <div class="text-h6">Documentation</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <i>Write the Documentation</i>
+          <br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit voluptatem distinctio hic accusamus, id aut odit cum ratione ullam similique ipsam! Temporibus dolorem consectetur molestiae labore quia tenetur deleniti a.
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "fullscreen-dialog",
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    closeDialog() {
+      this.$emit("closeFullscreen");
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,81 +4,36 @@
       <q-toolbar class="toolbar">
         <q-toolbar-title>Accounting</q-toolbar-title>
 
-        <q-btn flat @click="dialog = true">
+        <q-btn flat @click="isOpenFullscreen = true">
           <img src="https://img.icons8.com/material-outlined/24/000000/documents--v2.png" />
         </q-btn>
       </q-toolbar>
     </q-header>
     <br />
     <q-page-container>
-      <q-dialog
-        v-model="dialog"
-        persistent
-        :maximized="maximizedToggle"
-        transition-show="slide-up"
-        transition-hide="slide-down"
-      >
-        <q-card class="text-white" style="background: #f44336;
-  font-family: 'Acme', sans-serif;">
-          <q-bar>
-            <q-space />
-
-            <q-btn
-              dense
-              flat
-              icon="minimize"
-              @click="maximizedToggle = false"
-              :disable="!maximizedToggle"
-            >
-              <q-tooltip v-if="maximizedToggle" content-class="bg-white text-primary">Minimize</q-tooltip>
-            </q-btn>
-            <q-btn
-              dense
-              flat
-              icon="crop_square"
-              @click="maximizedToggle = true"
-              :disable="maximizedToggle"
-            >
-              <q-tooltip v-if="!maximizedToggle" content-class="bg-white text-primary">Maximize</q-tooltip>
-            </q-btn>
-            <q-btn dense flat icon="close" v-close-popup>
-              <q-tooltip content-class="bg-white text-primary">Close</q-tooltip>
-            </q-btn>
-          </q-bar>
-
-          <q-card-section>
-            <div class="text-h6">Documentation</div>
-          </q-card-section>
-
-          <q-card-section
-            class="q-pt-none"
-          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.</q-card-section>
-        </q-card>
-      </q-dialog>
+      <full-screen-dialog :isOpen="isOpenFullscreen" @closeFullscreen="isOpenFullscreen = false"></full-screen-dialog>
       <home-cards />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
+import FullScreenDialog from "components/ui/FullScreenDialog.vue";
 import HomeCards from "components/HomeCards.vue";
-
 export default {
   name: "MainLayout",
   data() {
     return {
-      dialog: false,
-      maximizedToggle: true,
+      isOpenFullscreen: false,
     };
   },
-  components: { HomeCards },
+  components: { FullScreenDialog, HomeCards },
 };
 </script>
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Acme&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Acme&display=swap");
 .toolbar {
   background: #ba000d;
-  font-family: 'Acme', sans-serif;
+  font-family: "Acme", sans-serif;
 }
-
 </style>
